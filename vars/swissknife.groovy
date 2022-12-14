1064caacc3fc3f6ba6ca5d3b5dcb5cef56431761
1Deepesh <56162357+1Deepesh@users.noreply.github.com>
2022-12-14 17:53:26 +0530

    Update swissknife.groovy

--- vars/swissknife.groovy
+++ vars/swissknife.groovy
@@ -1,3 +1,14 @@
 def printName(name){
 	println name
 }
+
+
+// note: to use this kind of function, we need to call in our jenkins file like below:
+
+// node() {
+// 	stage('a') {
+// 	  //nameofgroovyfile.functionname (parameter)
+// 	  swissKnife.printName ('John')	
+// 	}
+
+// }
