a3b39af30cc316090a29cb3a125d735ab671379b
1Deepesh <56162357+1Deepesh@users.noreply.github.com>
2022-12-14 16:28:34 +0530

    Update readConfigValue.groovy

--- vars/readConfigValue.groovy
+++ vars/readConfigValue.groovy
@@ -1,6 +1,6 @@
 def call (pathToFile) {
     if (pathToFile == "" || pathToFile == null ) {
-        pathToFile = "jenkinsdemo/resources/com/acceleratedskillup/feature-dev.yml"
+        pathToFile = load "jenkinsdemo/resources/com/acceleratedskillup/feature-dev.yml"
     }
     // read the yaml file
     raw_data = readYaml (file: 'pathToFile')
