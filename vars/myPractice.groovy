03bd9c341c22120586bff4284edf1fcafd5bfde0
1deepesh <1deepeshprasad@gmail.com>
2022-12-14 10:35:27 +0000

    dfhdfhdfhererer

---
+++ vars/myPractice.groovy
@@ -0,0 +1,24 @@
+def call (def String command = "build") {
+    if command == "" || command == null {
+        println ("please select an input")
+    } 
+    else if command == "build" {
+        pipeline {
+            agent any 
+            stages {
+                stage ('a') {
+                    println "====================================\n\t\texecute build\t\t\n================================="
+                }
+            }
+            post {
+                always {
+                    echo "The build got executed successfully, now you can send emails also to the stake holders."
+                }
+            }
+        }        
+    }
+    else {
+        println ("please select a valid line")
+    }
+    
+}
