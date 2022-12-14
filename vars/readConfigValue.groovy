69c0dc47a3aca175135ed7f268feb151e6def1bf
1Deepesh <56162357+1Deepesh@users.noreply.github.com>
2022-12-14 17:11:07 +0530

    Update readConfigValue.groovy

--- vars/readConfigValue.groovy
+++ vars/readConfigValue.groovy
@@ -1,8 +1,9 @@
 def call () {
     def pathToFile = libraryResource 'com/acceleratedskillup/feature-dev.yml'
+    println (pathToFile)
     
     // read the yaml file
-    raw_data = readYaml (file: 'pathToFile')
+    raw_data = readYaml (file: pathToFile)
     config_data = raw_data.environment 
     println (name)
 
